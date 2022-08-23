550b322b5c07f39fc38c8a639aa83422e7279991
kitorosano <rosanoesteban2@gmail.com>
2022-08-23 17:06:04 -0300

    Preparando env

--- src/main/java/taller1/Persistencia/ConexionDB.java
+++ src/main/java/taller1/Persistencia/ConexionDB.java
@@ -1,4 +1,24 @@
 package main.java.taller1.Persistencia;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionDB {
+    public static Connection getConnection() {
+        Connection connection = null;
+        String url = System.getProperty("GESTOREMPLEADOS_DB_URL");
+        String user = System.getProperty("GESTOREMPLEADOS_DB_USER");
+        String password = System.getProperty("GESTOREMPLEADOS_DB_PASS");
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            if (connection != null)
+                System.out.println("Conexion a la base de datos establecida");
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al conectar con la base de datos", e);
+        }
+        return connection;
+    }
+
 }

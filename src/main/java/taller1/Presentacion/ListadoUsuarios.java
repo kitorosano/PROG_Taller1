904a28c675fc9889d4b7968e4d67358c42bb9584
sstelmaj <seba.stelmaj21@gmail.com>
2022-08-26 17:25:37 -0300

    listar usuario hecho

--- src/main/java/taller1/Presentacion/ListadoUsuarios.java
+++ src/main/java/taller1/Presentacion/ListadoUsuarios.java
@@ -6,8 +6,10 @@ import main.java.taller1.Logica.Fabrica;
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,18 +25,13 @@ public class ListadoUsuarios {
 
     public ListadoUsuarios(){
         listaUsuarios.setModel(model);
-        //cargarLista();                    Hay que descomentar esto una vez esté finalizado el obtenerUsuario()
-        model.addElement("prueba1");        //borrar esto (es para probar)
-        model.addElement("prueba2");        //borrar esto (es para probar)
-        model.addElement("prueba3");        //borrar esto (es para probar)
-
+        cargarLista();
         listaUsuarios.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
                 if (e.getClickCount()==2){
-                    System.out.println("hola");
-                    //DetalleUsuario()          Hay que descomentar esto una vez esté finalizado el DetalleUsuario()
+                    llamarDetalle();
                 }
             }
         });
@@ -42,15 +39,30 @@ public class ListadoUsuarios {
 
     private void cargarLista(){
         Map<String, Usuario> usuarios = new HashMap<String, Usuario>();
-
+        model.clear();
         try {
             usuarios = Fabrica.getInstance().getIUsuario().obtenerUsuarios();
+            for (Map.Entry<String, Usuario> entry : usuarios.entrySet()) {
+                model.addElement(entry.getValue().getNickname());           //guardo el nickname en la lista
+            }
         }catch (Exception e){
             JOptionPane.showMessageDialog(null, "Error al cargar la lista" + e.toString());
         }
+    }
 
-        for (Map.Entry<String, Usuario> entry : usuarios.entrySet()) {
-            model.addElement(entry.getValue().getNickname());
+
+    private void llamarDetalle(){
+        Map<String, Usuario> usuarios = new HashMap<String, Usuario>();
+
+        try {
+            usuarios = Fabrica.getInstance().getIUsuario().obtenerUsuarios();
+            Usuario seleccionado = usuarios.get(listaUsuarios.getSelectedValue());  //Guardo el usuario seleccionado buscando en la lista por su nickname
+            JFrame detalle = new DetalleUsuario(seleccionado);        //Descomentar linea
+            detalle.setVisible(true);                                 //Descomentar linea
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Error al llamar al detalle usuario" + e.toString());
         }
     }
 }
+
+

55e86aea33c4c1e552b949ca2eee65dc18c8cae3
JoaquinMaidana <jocomaidana11@gmail.com>
2022-09-01 16:28:57 -0300

    finalizado

--- src/main/java/taller1/Presentacion/DetalleFuncion.java
+++ src/main/java/taller1/Presentacion/DetalleFuncion.java
@@ -1,20 +1,40 @@
 package main.java.taller1.Presentacion;
 
-import main.java.taller1.Logica.Clases.Artista;
-import main.java.taller1.Logica.Clases.Espectador;
+import main.java.taller1.Logica.Clases.EspectadorRegistradoAFuncion;
 import main.java.taller1.Logica.Clases.Funcion;
-import main.java.taller1.Logica.Clases.Usuario;
+import main.java.taller1.Logica.Fabrica;
 
 import javax.swing.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import javax.swing.table.DefaultTableModel;
+import java.util.Map;
 
 public class DetalleFuncion {
     private JPanel mainPanel;
     private JTextArea textArea1;
+    private JTextArea nombreContenido;
+    private JTextArea espectaculoContenido;
+    private JTextArea fechaHoraDeInicioContenido;
+    private JTextArea fechaDeRegistroContenido;
+    private JTextArea espectaculoTextArea;
+    private JTextArea fechaYHoraDeTextArea;
+    private JTextArea fechaDeRegistroTextArea;
+    private JTextArea detalleDeFuncionTextArea;
+    private JTable table1;
+    private JTextArea espectadoresTextArea;
+    private JScrollPane scrollPane1;
+    private JButton registrarEspectadoresButton;
 
+    Funcion funcion;
+
+    Map<String, EspectadorRegistradoAFuncion> espectadoresDeFuncion;
     public DetalleFuncion(Funcion funcion){
-        textArea1.setText(funcion.getNombre());
+        this.funcion=funcion;
+        nombreContenido.setText(this.funcion.getNombre());
+        espectaculoContenido.setText(this.funcion.getEspectaculo().getNombre());
+        fechaHoraDeInicioContenido.setText(this.funcion.getFechaHoraInicio().toString());
+        fechaDeRegistroContenido.setText(this.funcion.getFechaRegistro().toString());
+        createUIComponents();
+        cargarTabla();
     }
     public  JPanel getMainPanel(){return mainPanel;}
 
@@ -28,6 +48,31 @@ public class DetalleFuncion {
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        table1.setModel(new DefaultTableModel(null, new String[]{"Nickname","Correo"}) {
+                            @Override
+                            public boolean isCellEditable(int row, int column) {
+                                return false;
+                            }
+                        }
+        );
+        table1.getTableHeader().setReorderingAllowed(false);
+        table1.getTableHeader().setResizingAllowed(false);
+    }
+    private void cargarTabla() {
+
+        DefaultTableModel model = (DefaultTableModel) table1.getModel();
+        try {
+            this.espectadoresDeFuncion = Fabrica.getInstance().getIUsuario().obtenerEspectadoresRegistradosAFuncion(this.funcion.getNombre());
+            for (Map.Entry<String, EspectadorRegistradoAFuncion> entry : this.espectadoresDeFuncion.entrySet()) {
+                model.addRow(new Object[]{entry.getValue().getEspectador().getNickname(),entry.getValue().getEspectador().getCorreo()});
+            }
+        } catch (Exception exc) {
+            JOptionPane.showMessageDialog(null, "Error" + exc.toString());
+        }
     }
 }
 
--- src/main/java/taller1/Presentacion/Main.java
+++ src/main/java/taller1/Presentacion/Main.java
@@ -1,12 +1,9 @@
 package main.java.taller1.Presentacion;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import main.java.taller1.Logica.Clases.Espectador;
-import main.java.taller1.Logica.Clases.EspectadorRegistradoAFuncion;
-import main.java.taller1.Logica.Clases.Usuario;
+import main.java.taller1.Logica.Clases.*;
 import main.java.taller1.Logica.Fabrica;
 import main.java.taller1.Logica.Interfaces.IUsuario;
-import main.java.taller1.Logica.Clases.Artista;
 import main.java.taller1.Logica.Clases.Espectador;
 import main.java.taller1.Logica.Clases.Usuario;
 
@@ -21,12 +18,7 @@ public class Main {
                 .filename(".env")
                 .systemProperties()
                 .load();
-
-        //LocalDate fecha = LocalDate.now();
-        //LocalDateTime ldt = LocalDateTime.now();
-        //Usuario espectador =new Espectador("Ramiro11","Ramiro","Fernandez","ramiro11@mail.com",fecha);
-        //Usuario artista =new Artista("Duki","Mauro","Lombardo","duki@mail.com",fecha,"un gran artista","bio","url");
-        //DetalleUsuario.crearDetalleUsuario(espectador);
+        
         JFrame frame = new Dashboard("Corona Tickets UY");
         frame.setVisible(true);
     }

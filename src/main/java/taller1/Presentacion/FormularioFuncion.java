12e4892c5a1f7e7933459d030af89ffc6b4b992b
paulo-ruiz <paulo.ruizanderson@gmail.com>
2022-09-02 02:12:06 -0300

    Estructura base terminada

--- src/main/java/taller1/Presentacion/FormularioFuncion.java
+++ src/main/java/taller1/Presentacion/FormularioFuncion.java
@@ -1,4 +1,18 @@
 package main.java.taller1.Presentacion;
 
+import javax.swing.*;
+
 public class FormularioFuncion {
+    private JPanel panel1;
+    private JComboBox cbPlataforma;
+    private JComboBox cbEspectaculo;
+    private JTextField tfNombre;
+    private JList listaAinvitar;
+    private JButton agregarArtistaButton;
+    private JButton eliminarArtistaButton;
+    private JButton agregarFuncionButton;
+    private JButton cancelarButton;
+    private JList listaInvitados;
+    private JTextField tfFecha;
+    private JTextField tfHora;
 }

bd123326672ca62a80ddb1eeee790a8391430c3b
paulo-ruiz <paulo.ruizanderson@gmail.com>
2022-09-07 23:28:50 -0300

    Formulario terminado

--- src/main/java/taller1/Presentacion/FormularioRegistroEspectadorAFuncion.java
+++ src/main/java/taller1/Presentacion/FormularioRegistroEspectadorAFuncion.java
@@ -25,29 +25,37 @@ public class FormularioRegistroEspectadorAFuncion extends JFrame {
     private JButton cancelarButton;
     private JButton confirmarButton;
     private JScrollPane tablaFunciones;
+    private JList listaRegistPrevios;
 
-    private String plataformaSelect, espectaculoSelect, nombreFuncion;
-    Map<String, Funcion> funciones;
-    Map<String,Usuario>usuarios;
+    private String plataformaSelect, espectaculoSelect;
+    private String nombreFuncion;
+    private Map<String, Funcion> funciones;
+    private Map<String,Usuario>usuarios;
+
+    private Funcion funcionPredefinida;
+
+    private Map<String,EspectadorRegistradoAFuncion> espectadores=new HashMap<>();
 
     private DefaultListModel<String> modelAInvitar = new DefaultListModel<String>();
     private DefaultListModel<String> modelInvitados = new DefaultListModel<String>();
+
+    private DefaultListModel<String> modelRegistros = new DefaultListModel<String>();
     public FormularioRegistroEspectadorAFuncion(String title, Funcion funcion){
         super(title);
         setContentPane(panel1);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         pack();
         createUIComponents();
-        cargarDatosComboBox();
-        cargarDatosListas();
+        funcionPredefinida=funcion;
         confirmarButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 try {
-                    Fabrica.getInstance().getIEspectaculo().registrarEspectadoresAFunciones(AgregarEspectadores());
-                    JOptionPane.showMessageDialog(null,"espectadores agregados");
+                    Fabrica.getInstance().getIEspectaculo().registrarEspectadoresAFunciones(espectadores);
+                    JOptionPane.showMessageDialog(null,"Espectadores agregados");
+                    dispose();
                 } catch (Exception ex) {
-                    throw new RuntimeException(ex);
+                    JOptionPane.showMessageDialog(null,ex);
                 }
             }
         });
@@ -58,6 +66,7 @@ public class FormularioRegistroEspectadorAFuncion extends JFrame {
                 if (e.getSource() == cbPlataforma) {
                     plataformaSelect = (String) cbPlataforma.getSelectedItem();
                     try{
+                        cbEspectaculo.removeAllItems();
                         espectaculos = Fabrica.getInstance().getIEspectaculo().obtenerEspectaculos(plataformaSelect);
                         for (Espectaculo esp : espectaculos.values()) {
                             cbEspectaculo.addItem(esp.getNombre());
@@ -86,16 +95,24 @@ public class FormularioRegistroEspectadorAFuncion extends JFrame {
                     System.out.println("No se selecciono una fila");
                 } else {
                     nombreFuncion = (String) table1.getValueAt(fila, 0);
+                    cargarDatosListas();
                 }
+            }
+        });
+        listaAinvitar.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                cargarListaRegistros();
             }
         });
         agregarEspectadorButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 Espectaculo espectaculo=funciones.get(nombreFuncion).getEspectaculo();
-                if(modelInvitados.capacity()>=espectaculo.getMaxEspectadores()){
+                if(modelInvitados.getSize()>=espectaculo.getMaxEspectadores()){
                     JOptionPane.showMessageDialog(null,"No se puede agregar, capacidad maxima alcanzada");
                 }else{
+                    AgregarEspectadorAMapa();
                     String invitado=modelAInvitar.getElementAt(listaAinvitar.getSelectedIndex());
                     modelInvitados.addElement(invitado);
                     modelAInvitar.remove(listaAinvitar.getSelectedIndex());
@@ -105,28 +122,42 @@ public class FormularioRegistroEspectadorAFuncion extends JFrame {
         eliminarEspectadorButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
+                espectadores.remove((String)listaInvitados.getSelectedValue());
                 String invitadoE=modelInvitados.getElementAt(listaInvitados.getSelectedIndex());
                 modelAInvitar.addElement(invitadoE);
                 modelInvitados.remove(listaInvitados.getSelectedIndex());
             }
         });
+        if(funcion==null){
+            cargarDatosComboBox();
+        }else{
+            cbPlataforma.addItem(funcion.getEspectaculo().getPlataforma().getNombre());
+            cbEspectaculo.addItem(funcion.getEspectaculo().getNombre());
+            cargarTabla();
+        }
+        cancelarButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                dispose();
+            }
+        });
     }
-
-    private Map<String, EspectadorRegistradoAFuncion> AgregarEspectadores() {
-        Map<String,EspectadorRegistradoAFuncion> espectadores=new HashMap<>();
+    private void AgregarEspectadorAMapa(){
         Espectador nuevo;
+        double costo;
         Espectaculo espectaculo=funciones.get(nombreFuncion).getEspectaculo();
         Funcion funcion=funciones.get(nombreFuncion);
         usuarios=Fabrica.getInstance().getIUsuario().obtenerUsuarios();
-        for(int i=0; i<modelInvitados.getSize();i++){
-            nuevo= (Espectador) usuarios.get(modelInvitados.get(i));
-            espectadores.put(modelInvitados.get(i),new EspectadorRegistradoAFuncion(nuevo,funcion,false,espectaculo.getCosto(), LocalDateTime.now()));
+        nuevo= (Espectador) usuarios.get((String) listaAinvitar.getSelectedValue());
+        if(listaRegistPrevios.getSelectedIndices().length == 3){
+            costo=0;
+        }else{
+            costo=espectaculo.getCosto();
         }
-        return espectadores;
+        //FALTA VER COMO ACTUALIZAR LOS DATOS DE LOS ESPECTACULOS CANJEADOS
+        espectadores.put(listaAinvitar.getSelectedValue().toString(),new EspectadorRegistradoAFuncion(nuevo,funcion,false,costo, LocalDateTime.now()));
     }
-
-
-    public void cargarDatosComboBox() {
+    private void cargarDatosComboBox() {
         try {
             Map<String, Plataforma> plataformas = Fabrica.getInstance().getIEspectaculo().obtenerPlataformas();
             for (Plataforma p : plataformas.values()) {
@@ -138,29 +169,35 @@ public class FormularioRegistroEspectadorAFuncion extends JFrame {
     }
 
     private void createUIComponents() {
-        table1.setModel(new DefaultTableModel(null, new String[]{"Nombre", "Espectaculo","Fecha y Hora"}) {
-                            @Override
-                            public boolean isCellEditable(int row, int column) {
-                                return false;
-                            }
-        });
         table1.getTableHeader().setReorderingAllowed(false);
         table1.getTableHeader().setResizingAllowed(false);
         listaAinvitar.setModel(modelAInvitar);
         listaInvitados.setModel(modelInvitados);
+        listaRegistPrevios.setModel(modelRegistros);
+        listaRegistPrevios.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
     }
 
     private void cargarTabla() {
         plataformaSelect = (String) cbPlataforma.getSelectedItem();
         espectaculoSelect = (String) cbEspectaculo.getSelectedItem();
-        DefaultTableModel model = (DefaultTableModel) table1.getModel();
-        try {
-            funciones=Fabrica.getInstance().getIEspectaculo().obtenerFuncionesDeEspectaculo(plataformaSelect, espectaculoSelect);
-            for (Map.Entry<String, Funcion> entry : funciones.entrySet()) {
-                model.addRow(new Object[]{entry.getValue().getNombre(), entry.getValue().getEspectaculo().getNombre(),entry.getValue().getFechaHoraInicio()});
+        table1.setModel(new DefaultTableModel(null, new String[]{"Nombre", "Espectaculo","Fecha y Hora"}) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
             }
-        } catch (Exception ex) {
-            JOptionPane.showMessageDialog(null, ex);
+        });
+        DefaultTableModel model = (DefaultTableModel) table1.getModel();
+        if(funcionPredefinida==null) {
+            try {
+                funciones = Fabrica.getInstance().getIEspectaculo().obtenerFuncionesDeEspectaculo(plataformaSelect, espectaculoSelect);
+                for (Map.Entry<String, Funcion> entry : funciones.entrySet()) {
+                    model.addRow(new Object[]{entry.getValue().getNombre(), entry.getValue().getEspectaculo().getNombre(), entry.getValue().getFechaHoraInicio()});
+                }
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, ex);
+            }
+        }else{
+            model.addRow(new Object[]{funcionPredefinida.getNombre(),funcionPredefinida.getEspectaculo().getNombre(),funcionPredefinida.getFechaHoraInicio()});
         }
     }
 
@@ -176,11 +213,27 @@ public class FormularioRegistroEspectadorAFuncion extends JFrame {
             }
             for(Usuario u:usuarios.values()){
                 if(u instanceof Espectador && !modelInvitados.contains(u.getNickname())){
-                    modelAInvitar.addElement(u.getNombre());
+                    modelAInvitar.addElement(u.getNickname());
                 }
             }
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Error al cargar la lista" + e.toString());
+        }
+    }
+
+    public void cargarListaRegistros(){
+        Map<String,EspectadorRegistradoAFuncion> registros;
+        modelRegistros.clear();
+        String invitado=modelAInvitar.getElementAt(listaAinvitar.getSelectedIndex());
+        try {
+            registros= Fabrica.getInstance().getIUsuario().obtenerFuncionesRegistradasDelEspectador(invitado);
+            for(EspectadorRegistradoAFuncion esp: registros.values()){
+                if(!esp.isCanjeado()){
+                    modelRegistros.addElement(esp.getFuncion().getNombre());
+                }
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,e);
         }
     }
 }

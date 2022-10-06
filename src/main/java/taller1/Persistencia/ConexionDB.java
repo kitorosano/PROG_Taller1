397f2e95c9d2d582ff6872f9f9815dbe554125c5
kitorosano <rosanoesteban2@gmail.com>
2022-10-06 17:07:08 -0300

    Nueva build del artifact; arreglado conexion a la BD

--- src/main/java/taller1/Persistencia/ConexionDB.java
+++ src/main/java/taller1/Persistencia/ConexionDB.java
@@ -11,12 +11,15 @@ public class ConexionDB {
         String user = System.getProperty("DB_USER");
         String password = System.getProperty("DB_PASS");
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(url, user, password);
             if (connection != null)
                 System.out.println("Conexion a la base de datos establecida");
 
         } catch (SQLException e) {
             throw new RuntimeException("Error al conectar a la base de datos", e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
         return connection;
     }
